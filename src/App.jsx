4f1d68a05a255918fa1c460ab4ab126712392ce1
Egbe Alexander <alexsucceeds@gmail.com>
2023-02-25 00:22:59 +0100

    made one todo component

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,6 @@ function App() {
     <div>
       <h1>My Todos</h1>
       <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Explore the full React course" />
     </div>
   );
 }
