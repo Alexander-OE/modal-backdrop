eac8131bde9cd01c079628190c80ab3c5d61a8cb
Egbe Alexander <alexsucceeds@gmail.com>
2023-02-24 21:07:55 +0100

    work on functionality for modal and backdrop

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,21 @@
-const Modal = () => {
-    return ( 
-        <div className="modal">
-            <p>Are you sure ?</p>
-            <button className="btn btn--alt">Cancel</button>
-            <button className="btn">Confirm</button>
-        </div>
-     );
-}
- 
+const Modal = (props) => {
+  const cancelHandler = () => {
+    props.onCancel();
+  };
+  const closeeHandler = () => {
+    props.onConfirm();
+  };
+  return (
+    <div className="modal">
+      <p>Are you sure ?</p>
+      <button className="btn btn--alt" onClick={cancelHandler}>
+        Cancel
+      </button>
+      <button className="btn" onClick={closeeHandler}>
+        Confirm
+      </button>
+    </div>
+  );
+};
+
 export default Modal;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,27 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 const Todo = (props) => {
-    function deleteHandler() {
-        
-    }
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+function closeModaleHandler() {
+    setModalIsOpen(false);
+}
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal onCancel={closeModaleHandler} onConfirm={closeModaleHandler} />}
+      {modalIsOpen && <Backdrop onCancel={closeModaleHandler}/>}
     </div>
   );
 };
